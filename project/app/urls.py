5a4ff01cc4cf19a6b39d86ad889e8fca03435895
Snehaleksa <166499017+Snehaleksa@users.noreply.github.com>
2024-10-04 12:52:36 +0530

    first

---
+++ project/app/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+
+
+urlpatterns = [
+    path('',views.index),
+    path('create-new-post',views.createPost),
+    path('getpost',views.GETAlPosts),
+    path('update',views.UpdatePost),
+    path('delete',views.DeletePost),
+    path('getpostbyid',views.getPostById),
+]
---
+++ project/app/views.py
@@ -0,0 +1,71 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Post
+from .serializers import PostSerializer
+
+# Create your views here.
+
+@api_view(['GET'])
+def index(request):
+    return Response({"Success":"The setup was successfull"})
+
+
+
+@api_view(['GET','POST'])
+def createPost(request):
+    data=request.data
+    serializer=PostSerializer(data=data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response({"success":"Post created successfully"},status=201)
+    else:
+        return Response(serializer.errors,status=400)
+
+
+
+@api_view(['GET'])
+def GETAlPosts(request):
+    get_posts=Post.objects.all()
+    serializer=PostSerializer(get_posts,many=True)
+    return Response(serializer.data)
+
+
+@api_view(['PUT'])
+def UpdatePost(request):
+    post_id=request.data.get('post_id')
+    get_new_title =request.data.get('title')
+    get_new_content =request.data.get('content')
+    try:
+        post =Post.objects.get(id=post_id)
+        if get_new_title:
+            post.title =get_new_title
+            if get_new_title:
+                post.content =get_new_content
+                post.save()
+            return Response({"Success":"The post was successfully updated"},status=200) 
+    except Post.DoesNotExist:
+        return Response({"Error":"The post does not exist"},status=404)   
+    
+
+@api_view(['DELETE'])
+def DeletePost(request):
+    post_id=request.data.get("post_id")
+    try:
+        post =Post.objects.get(id=post_id)
+        post.delete()
+        return Response({"Success":"The post was successfully deleted"},status=200) 
+    except Post.DoesNotExist:
+        return Response({"Error":"The post does not exist"},status=404)   
+    
+
+
+@api_view(['GET'])
+def getPostById(request):
+    post_id = request.data.get('post_id')
+    try:
+        post = Post.objects.get(id=post_id)
+        
+        return Response({"sucess":"The post was successfully updated"}, status=200)
+    except Post.DoesNotExist:
+        return Response({"error": "Post not found"}, status=404)
